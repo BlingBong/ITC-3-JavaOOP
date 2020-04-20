f97b3bc8a90d766d3342651fa6e04447d46cc84d
Rivano Ardiyan Taufiq Kurniawan <rivano.atk@gmail.com>
2020-04-20 23:55:03 +0700

    Initial commit

---
+++ Java OOP/src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+//        Mahasiswa vano = new Mahasiswa("NHC", "12390000", 4.0);
+////        vano.nama = "Rivano";
+////        vano.nim = "123190084";
+////        vano.IPK = 4.0;
+//
+//        vano.setNama("Rivano");
+//        vano.setNim("123190084");
+//        vano.setIPK(4.0);
+//
+////        System.out.println("Nama = " + vano.nama);
+////        System.out.println("NIM = " + vano.nim);
+////        System.out.println("IPK = " + vano.IPK);
+//        System.out.println("Nama = " + vano.getNama());
+//        System.out.println("NIM = " + vano.getNim());
+//        System.out.println("IPK = " + vano.getIPK());
+//        vano.belajar();
+//        vano.doTugas();
+
+        Manusia manusia = new Manusia("Ricardo");
+        Mahasiswa mahasiswa = new Mahasiswa("Powfu","123190420",3.0);
+        mahasiswa.ktp = "1245367890";
+//        System.out.println("Nama : " + mahasiswa.nama);
+
+//        manusia.makan("Peneta");
+
+        mahasiswa.berjoget("TikTok");
+    }
+}
